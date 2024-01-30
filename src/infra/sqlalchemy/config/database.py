9ea41d686db51673960fba2bb52c4b8046f49972
Guilherme Gonçalves <guilhermemarcosgoncalves14@gmail.com>
2024-01-30 17:21:29 -0300

    database set production

--- src/infra/sqlalchemy/config/database.py
+++ src/infra/sqlalchemy/config/database.py
@@ -5,9 +5,10 @@ import pymysql
 
 pymysql.install_as_MySQLdb()
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost/db_list"
-
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost/db_list"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:HAG5C6GgF2cbA1ded1g3GH3622FEae6E@monorail.proxy.rlwy.net:25483/railway"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
