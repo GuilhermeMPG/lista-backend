b438d14d9ff6f479889896837db0a863f072949a
Guilherme Gonçalves <guilhermemarcosgoncalves14@gmail.com>
2024-01-30 18:39:07 -0300

    init em todos os diretorios add sys.path.append

--- src/server.py
+++ src/server.py
@@ -1,7 +1,18 @@
+import sys
+import os
+
+# Obtém o caminho absoluto do diretório pai do diretório atual (onde está este script)
+caminho_projeto = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+# Adiciona o caminho do projeto ao sys.path
+sys.path.append(caminho_projeto)
+
+
 from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
-from infra.sqlalchemy.config.database import criar_db
+from src.infra.sqlalchemy.config.database import criar_db
 from routers import rotas_usuario, rotas_itemDesejos
+
 
 
 app = FastAPI()
