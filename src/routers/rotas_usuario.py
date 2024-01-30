8db53f3325f8948a6a854f62afc6ef2fc5eee726
Guilherme Gonçalves <guilhermemarcosgoncalves14@gmail.com>
2024-01-30 18:22:57 -0300

    volta dos path local e add init

--- src/routers/rotas_usuario.py
+++ src/routers/rotas_usuario.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter
 from fastapi import Depends, status, HTTPException
 from typing import List
 from sqlalchemy.orm import Session
-from src.infra.sqlalchemy.config.database import get_db
-from src.routers.auth_utils import obter_usuario_logado
+from infra.sqlalchemy.config.database import get_db
+from routers.auth_utils import obter_usuario_logado
 
-from src.schemas.schemas import LoginData, Usuario
-from src.infra.providers import token_provaider, hash_provaider
+from schemas.schemas import LoginData, Usuario
+from infra.providers import token_provaider, hash_provaider
 
-from src.infra.sqlalchemy.repositorios.repositorio_usuario import RepositorioUsuario
+from infra.sqlalchemy.repositorios.repositorio_usuario import RepositorioUsuario
 
 
 router = APIRouter()
